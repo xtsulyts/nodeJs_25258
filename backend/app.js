3096cf60ee31ec3901148844a06a062c45c1940f
xtsulyts <531xtsulyts@gmail.com>
2025-09-09 23:00:54 -0300

    servidor express corriendo ok.

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 //const { salonesTodos } = require('./src/baseDeDatos/baseDeDatos');
-const autenticacionRuta = require('./src/rutas/autenticacionRuta')
-const productosRuta = require('./src/rutas/productosRuta')
+// const autenticacionRuta = require('./src/rutas/autenticacionRuta')
+// const productosRuta = require('./src/rutas/productosRuta')
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 app.use(cors());
 
 app.use(express.json()); // Para parsear JSON
-app.use(express.urlencoded({ extended: true })); // Para formularios
+// app.use(express.urlencoded({ extended: true })); // Para formularios
 
-// Rutas
-app.use('/api/autenticacion', autenticacionRuta);
-app.use('/api/productos', productosRuta);
+// // Rutas
+// app.use('/api/autenticacion', autenticacionRuta);
+// app.use('/api/productos', productosRuta);
 // console.log('Ruta de autenticacion cargada:', !!autenticacionRuta);
 // Ruta de prueba
-app.get('/', (req, res) => {
-    res.json({ 
-        message: 'API node.js 25258',
-        endpoints: {
-            productos: '/api/productos',
-            producto_especifico: '/api/productos/:id'
-        }
-    });
-});
+// app.get('/', (req, res) => {
+//     res.json({ 
+//         message: 'API node.js 25258',
+//         endpoints: {
+//             productos: '/api/productos',
+//             producto_especifico: '/api/productos/:id'
+//         }
+//     });
+// });
 
 app.listen(3000, () => {
     console.log('Servidor escuchando en puerto 3000');
